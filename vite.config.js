0932979131fad8dc8be97a83bf9714a81a1a4aff
Duvan Melo <duvan.10dma@gmail.com>
2026-01-05 15:19:00 -0500

    Actualizar configuración de Vite para incluir el servidor y eliminar configuraciones obsoletas

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-root:'FRONTEND',
-
-  build: {outDir: 'dist/frontend',}
-
+  server: {
+    host: true, // 👈 ESTO ES CLAVE: Permite que el servidor sea accesible desde la red (IP)
+    port: 5173, // Puerto por defecto
+  }
 });
